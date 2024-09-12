5866ff30a830964536803397223d0eb445357f8a
Me <joshi.sujit020386@gmail.com>
2024-09-12 13:55:38 -0400

    Progress.

--- src/pages/MetricsPage.vue
+++ src/pages/MetricsPage.vue
@@ -35,7 +35,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import {computed, ref} from 'vue';
 import GridComponent from 'components/GridComponent.vue';
 import DateRangeComponent from 'components/DateRangeComponent.vue';
 
@@ -100,70 +100,74 @@ const gridOptions = [
   },
 ];
 
-const updateDashboard = (startDate: string, endDate: string) => {
-  console.log('start date is', startDate, 'end date is', endDate);
+const updateDashboard = (sd: string, ed: string) => {
+  console.log('start date is', sd, 'end date is', ed);
+  startDate.value = sd;
+  endDate.value = ed;
 };
 
-const options = {
-  title: {
-    text: 'Stacked Line',
-  },
-  tooltip: {
-    trigger: 'axis',
-  },
-  legend: {
-    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true,
-  },
-  toolbox: {
-    feature: {
-      saveAsImage: {},
+const options = computed(() => {
+  return {
+    title: {
+      text: startDate.value + ' --  ' + endDate.value,
     },
-  },
-  xAxis: {
-    type: 'category',
-    boundaryGap: false,
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-  },
-  yAxis: {
-    type: 'value',
-  },
-  series: [
-    {
-      name: 'Email',
-      type: 'line',
-      stack: 'Total',
-      data: [120, 132, 101, 134, 90, 230, 210],
+    tooltip: {
+      trigger: 'axis',
     },
-    {
-      name: 'Union Ads',
-      type: 'line',
-      stack: 'Total',
-      data: [220, 182, 191, 234, 290, 330, 310],
+    legend: {
+      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
     },
-    {
-      name: 'Video Ads',
-      type: 'line',
-      stack: 'Total',
-      data: [150, 232, 201, 154, 190, 330, 410],
+    grid: {
+      left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
     },
-    {
-      name: 'Direct',
-      type: 'line',
-      stack: 'Total',
-      data: [320, 332, 301, 334, 390, 330, 320],
+    toolbox: {
+      feature: {
+        saveAsImage: {},
+      },
     },
-    {
-      name: 'Search Engine',
-      type: 'line',
-      stack: 'Total',
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
+    xAxis: {
+      type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     },
-  ],
-};
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: 'Email',
+        type: 'line',
+        stack: 'Total',
+        data: [120, 132, 101, 134, 90, 230, 210],
+      },
+      {
+        name: 'Union Ads',
+        type: 'line',
+        stack: 'Total',
+        data: [220, 182, 191, 234, 290, 330, 310],
+      },
+      {
+        name: 'Video Ads',
+        type: 'line',
+        stack: 'Total',
+        data: [150, 232, 201, 154, 190, 330, 410],
+      },
+      {
+        name: 'Direct',
+        type: 'line',
+        stack: 'Total',
+        data: [320, 332, 301, 334, 390, 330, 320],
+      },
+      {
+        name: 'Search Engine',
+        type: 'line',
+        stack: 'Total',
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+      },
+    ],
+  }
+});
 </script>
